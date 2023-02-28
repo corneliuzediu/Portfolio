69759993f2780fd42cc50deb233d32998cc26b69
corneliuzediu <zediucorneliu@gmail.com>
2023-02-28 10:51:37 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopSectionComponent } from './top-section/top-section.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsSetComponent } from './skills-set/skills-set.component';
+import { ProjectsBoardComponent } from './projects-board/projects-board.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopSectionComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    SkillsSetComponent,
+    ProjectsBoardComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
